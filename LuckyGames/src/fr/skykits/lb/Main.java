881d8d216f43edcc07cd2f61adf463a45a208d98
karine thuaudet <karine thuaudet@karine.home>
2016-04-28 19:43:01 +0200

    Init Projet

---
+++ LuckyGames/src/fr/skykits/lb/Main.java
@@ -0,0 +1,19 @@
+package fr.skykits.lb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+		
+	}
+
+}
